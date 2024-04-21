f1d8ca40da5a73dbce68ff7258d021fbbb80bb0e
carlos canacue <carlosduardcar@gmail.com>
2024-04-21 18:47:27 -0500

    adding signup logic

---
+++ src/app/shared/global-constants.ts
@@ -0,0 +1,18 @@
+export class GlobalConstants{
+    
+    //Mesage
+    public static genericError: string = "something went wrong, please try again later";
+
+    //Regex
+
+    public static nameRegex:string = "[a-zA-Z0-9 ]*";
+
+    public static emailRegex:string = "[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,3}";
+
+    public static contactNumberRegex:string = "^[e0-9]{10,10}$";
+
+    //variable
+    public static error:string = "error";
+
+
+}
