c482d5e675ab30b44017970030073dd6d3b12fbf
carlos canacue <carlosduardcar@gmail.com>
2024-04-29 11:47:11 -0500

    change loader stop place in code

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -46,9 +46,9 @@ export class LoginComponent implements OnInit {
       localStorage.setItem('token',response.token);
       this.router.navigate(['/cafe/dashboard']);
     },error=>{
+      this.ngxService.stop();
       if(error.error?.message){
         this.responseMessage = error.error?.message;
-        this.ngxService.stop();
       }else{
         this.responseMessage = GlobalConstants.genericError
       }
