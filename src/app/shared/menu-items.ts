59a3dc665b1d4fef9937dd1b78cf511d6ab6f8c2
carlos canacue <carlosduardcar@gmail.com>
2024-04-30 16:36:29 -0500

    add 'Manage Products' tab

--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -10,7 +10,8 @@ export interface Menu{
 
 const MANUITEMS = [
     {state:'dashboard', name:'Dashboard', type:'link', icon:'dashboard',role:''},
-    {state:'category', name:'Manage Category', type:'link', icon:'caregory',role:'admin'}
+    {state:'category', name:'Manage Category', type:'link', icon:'category',role:'admin'},
+    {state:'product', name:'Manage Products', type:'link', icon:'inventory_2',role:'admin'}
 ]
 
 @Injectable()
