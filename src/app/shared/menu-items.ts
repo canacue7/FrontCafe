825feb627f0fcbbd51ae65123bd7f8b9b9cd0840
carlos canacue <carlosduardcar@gmail.com>
2024-05-04 14:21:35 -0500

    added table

--- src/app/shared/menu-items.ts
+++ src/app/shared/menu-items.ts
@@ -12,7 +12,9 @@ const MANUITEMS = [
     {state:'dashboard', name:'Dashboard', type:'link', icon:'dashboard',role:''},
     {state:'category', name:'Manage Category', type:'link', icon:'category',role:'admin'},
     {state:'product', name:'Manage Products', type:'link', icon:'inventory_2',role:'admin'},
-    {state:'order', name:'Manage Order', type:'link', icon:'shopping_cart',role:''}
+    {state:'order', name:'Manage Order', type:'link', icon:'shopping_cart',role:''},
+    {state:'bill', name:'View Bill', type:'link', icon:'backup_table',role:''}
+
 
 ]
 
